c62ae97ff477b11ff94fb3a8b717279e0ee4ffe3
isyuck <kaazification@gmail.com>
2021-01-31 12:20:33 +0000

    rework structure to accommodate header images as well as video galleries, also introduce tags

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/static/gif`,
+        path: `${__dirname}/static/vid`,
         name: `assets`,
       },
     },

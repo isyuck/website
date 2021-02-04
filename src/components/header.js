837ba61eb0db32bcefb72e1a6d47c67085382a33
isyuck <kaazification@gmail.com>
2021-02-04 10:59:31 +0000

    add inner margin parameters

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,11 @@
 import React from "react"
 import PI from "../components/pi"
 
-const Header = ({ link, children }) => {
+const Header = ({ link, children, my = 2, mx = 2 }) => {
   return (
     <a
       href={link}
-      className="fixed m-2 z-30 h-9 text-white inset-0 text-xl flex flex-row bg-black space-x-4 justify-between"
+      className={`fixed my-${my} mx-${mx} z-30 h-9 text-white inset-0 text-xl flex flex-row bg-black space-x-4 justify-between`}
     >
       <div className="flex flex-row flex-shrink-0 px-2 py-1">
         <span>

da22e77da89fffce1bfa6eeb64fe238dc5b618c2
isyuck <kaazification@gmail.com>
2021-02-07 09:29:25 +0000

    update 404 page with link to index

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,8 +8,23 @@ const NotFoundPage = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <div className="flex absolute inset-0 h-screen z-0">
-        <p className="m-auto">404</p>
+      <div className="container m-auto">
+        <div className="my-16 space-y-4 place-items-center text-center flex flex-col">
+          <span
+            className={`w-16 font-mont rounded-full px-3 pb-0.5 text-lg bg-gradient-to-r from-gray-300 via-purple-200 to-gray-400`}
+          >
+            404
+          </span>
+          <span>you're lost! or i broke something</span>
+          <a
+            href="/"
+            className="mx-4 text-xl font-mond underline"
+            style={{ color: "#0000ff" }}
+          >
+            Go Home
+          </a>
+          <span>❓</span>
+        </div>
       </div>
     </Layout>
   )

f781af82ceb6da9b3cf54f023d8f6fa1958f1d2c
isaac <isaac@isaac.ac>
2021-03-04 17:08:51 +0000

    rework transform area, add new transforms, tidy

--- src/pages/emojis.jsx
+++ src/pages/emojis.jsx
@@ -1,155 +1,249 @@
-import React, { useRef, useState, useEffect, PropTypes } from "react";
-import Header from "../components/header";
-import Slider from "react-input-slider";
+import React, { useState, useEffect, useRef } from "react";
 import Control from "../components/emojis/control";
+import { Helmet } from "react-helmet";
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from "body-scroll-lock";
 
 const Emoji = ({ data, location }) => {
   const [ctrlValues, setCtrlValues] = useState([
     {
       id: 0,
-      name: "count",
-      value: 30,
+      name: "emoji count",
+      value: 15,
+      defaultVal: 15,
       minVal: 1,
       maxVal: 90,
       step: 1,
-      active: true,
       comp: "SliderX",
     },
     {
       id: 1,
-      name: "size",
+      name: "base size",
       value: 200,
+      defaultVal: 200,
       minVal: 10,
       maxVal: 500,
       step: 3,
-      active: true,
       comp: "SliderX",
     },
     {
       id: 2,
-      name: "gamma",
-      value: 2.5,
+      name: "gamma/spin",
+      value: 0.7,
+      defaultVal: 0.7,
       minVal: 0,
-      maxVal: 10,
+      maxVal: 1,
       step: 0.01,
-      active: true,
       comp: "SliderX",
     },
     {
       id: 3,
-      name: "saturation",
-      value: 100,
+      name: "spin speed",
+      value: 1.5,
+      defaultVal: 1.5,
       minVal: 0,
-      maxVal: 500,
-      step: 1,
-      active: true,
+      maxVal: 10,
+      step: 0.1,
       comp: "SliderX",
     },
     {
       id: 4,
-      name: "blur step",
-      value: 0,
+      name: "step",
+      defaultVal: 3,
+      value: 3,
       minVal: 0,
-      maxVal: 3,
-      step: 0.01,
-      active: true,
+      maxVal: 10,
+      step: 0.1,
       comp: "SliderX",
     },
     {
       id: 5,
-      name: "hue step",
-      value: 0,
+      name: "saturation",
+      defaultVal: 100,
+      value: 100,
       minVal: 0,
-      maxVal: 20,
-      step: 0.1,
-      active: true,
+      maxVal: 500,
+      step: 1,
       comp: "SliderX",
     },
     {
       id: 6,
-      name: "invert",
+      name: "blur step",
+      defaultVal: 0,
       value: 0,
       minVal: 0,
-      maxVal: 100,
-      step: 1,
-      active: true,
+      maxVal: 1,
+      step: 0.01,
       comp: "SliderX",
     },
     {
       id: 7,
-      name: "opacity",
-      value: 100,
+      name: "hue step",
+      defaultVal: 0,
+      value: 0,
       minVal: 0,
-      maxVal: 100,
-      step: 1,
-      active: true,
+      maxVal: 20,
+      step: 0.1,
       comp: "SliderX",
     },
     {
       id: 8,
-      name: "brightness",
-      value: 100,
+      name: "invert",
+      defaultVal: 0,
+      value: 0,
       minVal: 0,
-      maxVal: 300,
+      maxVal: 100,
       step: 1,
-      active: true,
       comp: "SliderX",
     },
     {
       id: 9,
-      name: "contrast step",
+      name: "opacity",
+      defaultVal: 100,
       value: 100,
       minVal: 0,
-      maxVal: 300,
+      maxVal: 100,
       step: 1,
-      active: true,
       comp: "SliderX",
     },
     {
       id: 10,
-      name: "skew",
-      value: { x: 0.5, y: 0.5 },
-      minVal: -2,
-      maxVal: 2,
-      step: 0.01,
-      active: true,
-      comp: "SliderXY",
+      name: "brightness",
+      defaultVal: 100,
+      value: 100,
+      minVal: 0,
+      maxVal: 300,
+      step: 1,
+      comp: "SliderX",
     },
     {
       id: 11,
-      name: "scale",
-      value: { x: 0.5, y: 0.5 },
-      minVal: -1,
-      maxVal: 1,
-      step: 0.001,
-      active: true,
-      comp: "SliderXY",
+      name: "contrast step",
+      defaultVal: 100,
+      value: 100,
+      minVal: 0,
+      maxVal: 300,
+      step: 1,
+      comp: "SliderX",
+    },
+    {
+      id: 12,
+      name: "skew weight",
+      defaultVal: 0,
+      value: 0,
+      minVal: 0,
+      maxVal: 2,
+      step: 0.01,
+      comp: "SliderX",
+    },
+    {
+      id: 13,
+      name: "skewX",
+      defaultVal: 200,
+      value: 0,
+      minVal: -2,
+      maxVal: 2,
+      step: 0.01,
+      comp: "SliderX",
+    },
+    {
+      id: 14,
+      name: "skewY",
+      defaultVal: 0,
+      value: 0,
+      minVal: -2,
+      maxVal: 2,
+      step: 0.01,
+      comp: "SliderX",
+    },
+    {
+      id: 15,
+      name: "scale weight",
+      defaultVal: 0,
+      value: 0,
+      minVal: 0,
+      maxVal: 2,
+      step: 0.01,
+      comp: "SliderX",
+    },
+    {
+      id: 16,
+      name: "scaleX",
+      defaultVal: 0.5,
+      value: 0.5,
+      minVal: -2,
+      maxVal: 2,
+      step: 0.01,
+      comp: "SliderX",
+    },
+    {
+      id: 17,
+      name: "scaleY",
+      defaultVal: 0.5,
+      value: 0.5,
+      minVal: -2,
+      maxVal: 2,
+      step: 0.01,
+      comp: "SliderX",
+    },
+    {
+      id: 18,
+      name: "translateX",
+      defaultVal: 0.0,
+      value: 0.0,
+      minVal: -200,
+      maxVal: 200,
+      step: 0.5,
+      comp: "SliderX",
+    },
+    {
+      id: 19,
+      name: "translateY",
+      defaultVal: 0.0,
+      value: 0.0,
+      minVal: -200,
+      maxVal: 200,
+      step: 0.5,
+      comp: "SliderX",
+    },
+    {
+      id: 20,
+      name: "translateZ",
+      defaultVal: 0.0,
+      value: 0.0,
+      minVal: -50,
+      maxVal: 50,
+      step: 1,
+      comp: "SliderX",
     },
   ]);
 
-  const [motionGranted, setMotionGranted] = useState(false);
-  const [gamma, setGamma] = useState(0);
-
-  // 'freeze' input from device rotation
-  const [freeze, setFreeze] = useState(false);
+  const mainBodyRef = useRef();
+  const ctrlRef = useRef();
 
   useEffect(() => {
-    document.body.style.overflow = "hidden";
+    disableBodyScroll(ctrlRef.current);
+    enableBodyScroll(mainBodyRef.current);
+  }, []);
+
+  // why doesn't js have linear interpolation?
+  const lerp = (v0, v1, t) => {
+    return v0 * (1 - t) + v1 * t;
+  };
+
+  const [motionGranted, setMotionGranted] = useState(false);
+
+  // phone rotation
+  const [gamma, setGamma] = useState(0);
+
+  useEffect(() => {
     if (window.DeviceOrientationEvent) {
       window.ondeviceorientation = (e) => handleOrientation(e);
     }
   });
-
-  const handleOrientation = (e) => {
-    if (!freeze) {
-      setGamma(e.gamma);
-    }
-  };
-
-  useEffect(() => {
-    setCtrlOpen(true);
-    setTimeout(setCtrlOpen(false), 3000);
-  }, []);
 
   const requestMotionAccess = () => {
     if (typeof DeviceOrientationEvent.requestPermission === "function") {
@@ -166,6 +260,10 @@ const Emoji = ({ data, location }) => {
     }
   };
 
+  const handleOrientation = (e) => {
+    setGamma(e.gamma);
+  };
+
   const newEmoji = () => {
     setEmoji(emojis[Math.floor(Math.random() * emojis.length)].substring(0, 2));
   };
@@ -175,174 +273,125 @@ const Emoji = ({ data, location }) => {
 
   const [ctrlOpen, setCtrlOpen] = useState(false);
 
-  const updateOnCtrlChange = (index, value, active) => {
-    let cv = [...ctrlValues];
-    cv[index].value = value;
-    cv[index].active = active;
-    setCtrlValues(cv);
-  };
-
-  const emojiInput = useRef(null);
-
   return (
     <>
-      <div className="fixed flex inset-0 w-screen h-screen pointer-events-none z-0">
-        <div
-          onClick={() => newEmoji()}
-          className="m-auto w-2/3 h-2/3 pointer-events-auto"
-        ></div>
-      </div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>emojis | isaac.ac</title>
+        <link rel="canonical" href="https://isaac.ac" />
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Helmet>
       <div
         onClick={() => setCtrlOpen(!ctrlOpen)}
-        className="fixed top-0 left-0 right-0 flex flex-row justify-between text-2xl z-10 pt-2 px-4 items-end"
+        className="fixed top-0 left-0 right-0 flex flex-row justify-between text-2xl z-10 pt-2 px-4 items-end overflow-hidden"
       >
         <p className="underline font-mond pb-1" style={{ color: "#0000ff" }}>
           emojis
         </p>
-        {/* </p> */}
-
         <p className="italic text-xl pb-1 font-mont">{emoji}</p>
       </div>
       {!motionGranted && (
-        <div className="fixed flex inset-0 w-screen h-screen">
+        <div
+          id={"motion-access-button"}
+          className="fixed flex inset-0 w-screen h-screen"
+          style={{ zIndex: 75 }}
+        >
           <div className="text-center m-auto">
             <button
               onClick={() => requestMotionAccess()}
               className="mb-20 p-8 text-xl"
             >
               tap to allow motion and orientation
+              <p className="text-sm mt-8">(nothing bad will happen)</p>
             </button>
           </div>
         </div>
       )}
-      <div
-        className="absolute flex flex-row inset-0 w-screen h-screen transform duration-500 z-30"
-        style={{
-          transform: `${ctrlOpen ? "translateX(0)" : "translateX(calc(80vw))"}`,
-        }}
-      >
+      <div ref={mainBodyRef}>
         <div
-          onClick={() => setCtrlOpen(!ctrlOpen)}
-          className={`h-screen w-1/2 flex pointer-events-auto`}
-        ></div>
-        <Control ctrlValues={ctrlValues} setCtrlValues={setCtrlValues} />
-      </div>
-      {motionGranted && (
-        <div
-          className="absolute text-center max-h-screen h-screen transform duration-500 overflow-hidden pointer-events-none"
+          className="absolute flex flex-col inset-0 w-screen h-screen max-h-screen transform duration-500 z-30 overflow-hidden"
           style={{
-            fontSize: `${ctrlValues[1].value}px`,
             transform: `${
-              ctrlOpen ? "translateX(calc(-20vw - 0rem))" : "translateX(0)"
+              ctrlOpen ? "translateY(0)" : `translateY(calc(50vh - 3.5rem))`
             }`,
-            width: `${ctrlOpen ? "70vw" : "100vw"}`,
           }}
         >
-          <div className="flex relative h-screen w-screen">
-            {[...Array(ctrlValues[0].value)].map((x, i) => (
-              <button
-                className={`absolute my-auto z-0 h-full w-full overflow-hidden`}
-                style={{
-                  filter: `saturate(${ctrlValues[3].value}%)
-                         blur(${i * ctrlValues[4].value}px)
-                         hue-rotate(${i * ctrlValues[5].value}deg)
-                         invert(${ctrlValues[6].value}%)
-                         opacity(${ctrlValues[7].value}%)
-                         brightness(${ctrlValues[8].value}%)
-                         contrast(${ctrlValues[9].value}%)
+          <div
+            onClick={() => setCtrlOpen(!ctrlOpen)}
+            className={`h-1/2 w-screen flex pointer-events-auto`}
+          ></div>
+          <Control
+            childRef={ctrlRef}
+            ctrlValues={ctrlValues}
+            emoji={emoji}
+            newEmoji={newEmoji}
+          />
+        </div>
+        {motionGranted && (
+          <div
+            className="absolute text-center w-screen max-w-screen transform duration-500 overflow-hidden pointer-events-none"
+            style={{
+              fontSize: `${ctrlValues[1].value}px`,
+              transform: `${
+                ctrlOpen ? "translateY(calc(-20vh))" : "translateY(0)"
+              }`,
+              height: `${ctrlOpen ? "70vh" : "100vh"}`,
+            }}
+          >
+            <div style={{}} className="flex relative h-screen w-screen">
+              {[...Array(parseInt(ctrlValues[0].value))].map((x, i) => (
+                <button
+                  className={`absolute my-auto z-0 h-full w-full overflow-hidden`}
+                  style={{
+                    willChange: "transform",
+                    filter: `saturate(${ctrlValues[5].value}%)
+                         blur(${i * ctrlValues[6].value}px)
+                         hue-rotate(${i * ctrlValues[7].value}deg)
+                         invert(${ctrlValues[8].value}%)
+                         opacity(${ctrlValues[9].value}%)
+                         brightness(${ctrlValues[10].value}%)
+                         contrast(${ctrlValues[11].value}%)
 `,
-                  transform: `
-/* rotateY(${(gamma + i) * ctrlValues[2].value}deg) */
-rotateY(${Date.now() * 0.05 + i * 4}deg)
+                    transform: `
+rotateY(${
+                      lerp(
+                        (Date.now() * 0.05 + i) * ctrlValues[3].value,
+                        gamma + i,
+                        ctrlValues[2].value
+                      ) * ctrlValues[4].value
+                    }deg)
 ${
-  ctrlValues[10].active
-    ? ` skew(${i * ctrlValues[10].value.x}deg, ${
-        i * ctrlValues[10].value.y
+  ctrlValues[12].value > 0.0
+    ? ` skew(${i * ctrlValues[13].value * ctrlValues[12].value}deg, ${
+        i * ctrlValues[14].value * ctrlValues[12].value
       }deg)`
     : ""
 }
+translate3d(${ctrlValues[18].value}px, ${i * ctrlValues[19].value}px, ${
+                      i * ctrlValues[20].value
+                    }px)
 ${
-  ctrlValues[11].active
-    ? ` scale(${i * ctrlValues[11].value.x}, ${i * ctrlValues[11].value.y})`
+  ctrlValues[15].value > 0.0
+    ? ` scale(${i * ctrlValues[16].value * ctrlValues[15].value}, ${
+        i * ctrlValues[17].value * ctrlValues[15].value
+      })`
     : ""
 }
 `,
-                }}
-              >
-                {emoji}
-              </button>
-            ))}
+                  }}
+                >
+                  {emoji}
+                </button>
+              ))}
+            </div>
           </div>
-        </div>
-      )}
-    </>
-  );
-};
-
-const ControlSingle = ({
-  name,
-  onChange,
-  start,
-  min,
-  max,
-  step,
-  active,
-  hasCheckbox,
-}) => {
-  const [val, setVal] = useState(start);
-  const [compActive, setCompActive] = useState(active);
-
-  const handleChangeActive = (e) => {
-    setCompActive(e.target.checked);
-    onChange(val, e.target.checked);
-  };
-
-  const handleChangeSlider = (e) => {
-    setVal(e);
-    onChange(e, compActive);
-  };
-
-  return (
-    <div className={`relative flex flex-row h-6 text-gray-800`}>
-      <div>
-        <div
-          className={`absolute inset-0 px-1 pt-0.5 z-20 pointer-events-none flex font-mono text-sm flex-row justify-between`}
-        >
-          <span>{name}</span>
-          <span className="text-gray-400">{`${val.toFixed(2)}`}</span>
-        </div>
-        <div className={`absolute inset-0 w-full z-10`}>
-          <Slider
-            styles={{
-              track: {
-                backgroundColor: "#00000000",
-                width: "100%",
-                height: 24,
-                boxSizing: "border-box",
-                border: "1px solid #d1d5db",
-                borderRadius: 0,
-              },
-              active: {
-                width: "100%",
-                backgroundColor: "#d1d5db",
-                height: 23,
-                borderRadius: 0,
-              },
-              thumb: {
-                width: 0,
-                height: 0,
-              },
-            }}
-            axis="x"
-            x={val}
-            xmin={min}
-            xmax={max}
-            xstep={step}
-            onChange={({ x }) => handleChangeSlider(x)}
-          />
-        </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
